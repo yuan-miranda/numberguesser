c97037f83508d1f6ff2de2afb2d23c77b0337552
yuan-miranda <yuanezekielamiranda@gmail.com>
2024-01-31 09:46:17 +0800

    added lives so you dont infinitely guess

--- src/Main.kt
+++ src/Main.kt
@@ -2,17 +2,28 @@ import kotlin.random.Random
 
 fun main() {
     var randomNumber = Random.nextInt(1, 11)
+    var lives = 5
 
     while(true) {
-        println("Guess the number from 1-10!")
+        // check if lives is 0
+        if(lives == 0) {
+            println("Game over!")
+            return
+        }
+
+        // prompt the user to guess
+        println("($lives/5) Guess the number from 1-10!")
         val guess = readln().toIntOrNull()
 
+        // check if guess is valid number
         if(guess == null) {
             println("Guess must be a number.")
 
+        // check if guess is correct
         } else if(guess == randomNumber) {
             println("Your guess is correct!")
 
+            // play again?
             while(true) {
                 print("Continue? (Y/n)")
                 val choice = readln().lowercase()
@@ -21,17 +32,23 @@ fun main() {
                     return
                 } else if(choice == "y" || choice == "") {
                     randomNumber = Random.nextInt(1, 11)
+                    lives = 5
                     break
                 } else {
                     println("invalid input")
                     continue
                 }
             }
+        
+        // guess is lower
         } else if(guess < randomNumber) {
             println("Your guess is lower.")
+            lives--
 
+        // guess is higher
         } else {
             println("Your guess is higher.")
+            lives--
         }
     }
 }
